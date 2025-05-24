c6eeedce073e0b3611751bb6e03d4b6550a2ab76
Deniz <candenizozder08@gmail.com>
2025-05-24 05:47:24 +0300

    Live Test

---
+++ js/app.js
@@ -0,0 +1,21 @@
+document.getElementById("sign_btn").addEventListener("click", function discord(){
+     window.open("https://discord.gg/2sSz4aXSpd", "_blank");
+});
+
+// Scrolldan sonra navbarı saklamak
+let lastScrollTop = 0;
+const navbar = document.getElementById("header");
+
+window.addEventListener("scroll", function () {
+  const scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop && scrollTop > 50) {
+    // Aşağı kaydırılıyor → gizle
+    navbar.classList.add("hide-navbar");
+  } else {
+    // Yukarı kaydırılıyor → göster
+    navbar.classList.remove("hide-navbar");
+  }
+
+  lastScrollTop = scrollTop;
+});
